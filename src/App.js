dcd9a1c6186c0d2b3c30bdbd736fd8fd62cf269a
Duh Get Over it <34867097+Maharl1kans@users.noreply.github.com>
2023-08-01 11:18:16 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'MP Legions';
-  return <div>Hello {value}</div>;
+  const value = 'MP Legions Team';
+  return <div>Welcome to {value}</div>;
 }
 
 export default App;
